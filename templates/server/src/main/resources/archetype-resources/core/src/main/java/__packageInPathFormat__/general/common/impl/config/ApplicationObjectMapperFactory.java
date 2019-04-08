97ac9538ed4f910d9e17ee30def646d5cce7d825
vapadwal <vaibhav.padwal@capgemini.com>
2019-04-08 21:12:51 +0530

    my-thai-star/issues#152: Support for JSR310 (New Date/Time API) and other JSON improvements (#64)

--- templates/server/src/main/resources/archetype-resources/core/src/main/java/__packageInPathFormat__/general/common/impl/config/ApplicationObjectMapperFactory.java
+++ templates/server/src/main/resources/archetype-resources/core/src/main/java/__packageInPathFormat__/general/common/impl/config/ApplicationObjectMapperFactory.java
@@ -5,8 +5,11 @@ import javax.inject.Named;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.web.csrf.CsrfToken;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-
 import com.devonfw.module.json.common.base.ObjectMapperFactory;
 import com.devonfw.module.json.common.base.type.PageableJsonSerializer;
 import com.devonfw.module.json.common.base.type.PageableJsonDeserializer;
@@ -26,8 +29,28 @@ public class ApplicationObjectMapperFactory extends ObjectMapperFactory {
     // see https://github.com/devonfw-wiki/devon4j/wiki/guide-json#json-and-inheritance
     SimpleModule module = getExtensionModule();
     module.addAbstractTypeMapping(CsrfToken.class, CsrfTokenImpl.class);
-	// register spring-data Pageable
+    // register spring-data Pageable
     module.addSerializer(Pageable.class, new PageableJsonSerializer());
     module.addDeserializer(Pageable.class, new PageableJsonDeserializer());
+  }
+
+  /**
+   * override createInstance method.
+   */
+  @Override
+  public ObjectMapper createInstance() {
+
+    ObjectMapper objectMapper = super.createInstance();
+    // omit properties in JSON that are null
+    objectMapper.setSerializationInclusion(Include.NON_NULL);
+    // Write legacy date/calendar as readable text instead of numeric value
+    // See
+    // https://fasterxml.github.io/jackson-databind/javadoc/2.6/com/fasterxml/jackson/databind/SerializationFeature.html#WRITE_DATES_AS_TIMESTAMPS
+    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    // ignore unknown properties in JSON to prevent errors
+    // e.g. when the service has been updated/extended but the calling REST client is not yet updated
+    // see https://github.com/devonfw-wiki/devon4j/wiki/guide-service-layer#versioning
+    objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+    return objectMapper;
   }
 }
