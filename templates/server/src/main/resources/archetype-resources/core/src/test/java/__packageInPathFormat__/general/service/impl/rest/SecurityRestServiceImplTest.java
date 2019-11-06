c1815664aac42317f929d504078fc1f68de6d9d1
Jörg Hohwiller <hohwille@users.sourceforge.net>
2019-11-06 16:25:28 +0100

    #166: fixed archetype

--- templates/server/src/main/resources/archetype-resources/core/src/test/java/__packageInPathFormat__/general/service/impl/rest/SecurityRestServiceImplTest.java
+++ templates/server/src/main/resources/archetype-resources/core/src/test/java/__packageInPathFormat__/general/service/impl/rest/SecurityRestServiceImplTest.java
@@ -1,7 +1,6 @@
 package ${package}.general.service.impl.rest;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
