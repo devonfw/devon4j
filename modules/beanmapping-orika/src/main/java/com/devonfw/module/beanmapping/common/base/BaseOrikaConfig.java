fc65c26b765c9056fe5d716f775a86203531f0c5
CORP\vapadwal <vaibhav.padwal@capgemini.com>
2020-02-17 14:01:19 +0530

    updated review comments

--- modules/beanmapping-orika/src/main/java/com/devonfw/module/beanmapping/common/base/BaseOrikaConfig.java
+++ modules/beanmapping-orika/src/main/java/com/devonfw/module/beanmapping/common/base/BaseOrikaConfig.java
@@ -1,17 +1,34 @@
 package com.devonfw.module.beanmapping.common.base;
 
+import org.springframework.context.annotation.Bean;
+
 import com.devonfw.module.basic.common.api.entity.GenericEntity;
 import com.devonfw.module.basic.common.api.to.AbstractEto;
 import com.devonfw.module.beanmapping.common.impl.orika.CustomMapperEto;
 
+import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.DefaultMapperFactory;
 
 /**
- * Base class which will add Custom Mapping for Entity to ETO. The method
- * {@link #configureCustomMapping(MapperFactory)} can be overridden as per requirements
+ * Base class which will add Custom Mapping for Entity to ETO. The method {@link #configureCustomMapping(MapperFactory)}
+ * can be overridden as per requirements
  *
  */
 public class BaseOrikaConfig {
+
+  /**
+   * @return {@link MapperFacade}
+   */
+  @Bean
+  public MapperFacade getOrika() {
+
+    MapperFactory factory = new DefaultMapperFactory.Builder().build();
+    // {@link #configureCustomMapping(MapperFactory)} can be overridden as per requirements
+    MapperFacade orika = configureCustomMapping(factory).getMapperFacade();
+    return orika;
+  }
+
   /**
    * Custom mapping for Entity to ETO
    *
--- templates/server/src/main/resources/archetype-resources/core/src/main/java/__packageInPathFormat__/general/common/impl/config/BeansOrikaConfig.java
+++ templates/server/src/main/resources/archetype-resources/core/src/main/java/__packageInPathFormat__/general/common/impl/config/BeansOrikaConfig.java
@@ -1,31 +1,18 @@
 package ${package}.general.common.impl.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import ma.glasnost.orika.MapperFacade;
-import ma.glasnost.orika.MapperFactory;
-import ma.glasnost.orika.impl.DefaultMapperFactory;
 import com.devonfw.module.beanmapping.common.base.BaseOrikaConfig;
 
 /**
  * Java bean configuration for Orika
+ * 
+ *  {@link #configureCustomMapping(MapperFactory)} from {@link BaseOrikaConfig} can be overridden as per requirements
  */
 @Configuration
 @ComponentScan(basePackages = { "com.devonfw.module.beanmapping" })
 public class BeansOrikaConfig extends BaseOrikaConfig{
 
  
-  /**
-   * @return the {@link MapperFacade}.
-   */
-  @Bean
-  public MapperFacade getOrika() {
-
-    MapperFactory factory = new DefaultMapperFactory.Builder().build();
-    //{@link #configureCustomMapping(MapperFactory)} can be overridden as per requirements
-    MapperFacade orika = configureCustomMapping(factory).getMapperFacade();
-    return orika;
-  }
 }
