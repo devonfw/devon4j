651202d2b927aa5ccad4514c49a750e8cecebde9
CORP\vapadwal <vaibhav.padwal@capgemini.com>
2020-04-27 16:56:19 +0530

    update doc

--- modules/security-jwt/src/main/java/com/devonfw/module/security/jwt/common/base/JwtAuthenticationFilter.java
+++ modules/security-jwt/src/main/java/com/devonfw/module/security/jwt/common/base/JwtAuthenticationFilter.java
@@ -9,7 +9,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
