2e16a921cb54f20a01ebee4e7b8778a5664658a0
M <ravichandran.m@capgemini.com>
2020-05-29 16:51:09 +0530

    wip-integration tests

--- starters/starter-security-jwt/src/test/java/com/devonfw/module/security/jwt/common/base/kafka/KafkaMessageListener.java
+++ starters/starter-security-jwt/src/test/java/com/devonfw/module/security/jwt/common/base/kafka/KafkaMessageListener.java
@@ -1,9 +1,14 @@
 package com.devonfw.module.security.jwt.common.base.kafka;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+
+import com.devonfw.module.kafka.common.messaging.retry.api.client.MessageProcessor;
+import com.devonfw.module.kafka.common.messaging.retry.api.client.MessageRetryOperations;
 
 /**
  * A Listener class with {@link KafkaListener} listens the message for the given topic and group name.
@@ -12,18 +17,33 @@ import org.springframework.kafka.annotation.KafkaListener;
 @Named
 public class KafkaMessageListener {
 
+  @Inject
+  private MessageRetryOperations<String, String> messageRetryOperations;
+
+  @Inject
+  private MessageProcessor<String, String> messageProcessor;
+
   /**
-   * This method is used for the purpose of testing {@link JwtTokenValidationAspect}
+   * This method is used to listen the message in kafka broker for the given topic and group name in
+   * {@link KafkaListener} and also to process the consumed message.
    *
-   * @param consumerRecord the {@link ConsumerRecord}
-   * @return the {@link ConsumerRecord}
-   * @throws Exception the {@link Exception}
+   * @param consumerRecord the consumed {@link ConsumerRecord}
+   * @param acknowledgment the {@link Acknowledgment} to acknowledge the listener that message has been processed.
+   * @throws Exception exception
    */
   @JwtAuthentication(failOnMissingToken = true)
   @KafkaListener(topics = JwtTokenValidationAspectTest.TEST_TOPIC, groupId = "test-group")
-  public ConsumerRecord<String, String> consumer(ConsumerRecord<String, String> consumerRecord) throws Exception {
+  public void consumer(ConsumerRecord<String, String> consumerRecord, Acknowledgment acknowledgment) throws Exception {
 
-    return consumerRecord;
+    processMessageAndAcknowledgeListener(consumerRecord, acknowledgment);
   }
 
+  private void processMessageAndAcknowledgeListener(ConsumerRecord<String, String> consumerRecord,
+      Acknowledgment acknowledgment) {
+
+    this.messageRetryOperations.processMessageWithRetry(consumerRecord, this.messageProcessor);
+
+    // Acknowledge the listener.
+    acknowledgment.acknowledge();
+  }
 }
