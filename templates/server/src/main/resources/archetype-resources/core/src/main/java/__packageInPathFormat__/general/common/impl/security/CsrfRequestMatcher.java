822ef97fa92ca4aa9a8b393ad0f1933ea0090fe2
Jörg Hohwiller <hohwille@users.sourceforge.net>
2020-12-07 14:11:31 +0100

    #307: fixed CsrfRequestMatcher

--- templates/server/src/main/resources/archetype-resources/core/src/main/java/__packageInPathFormat__/general/common/impl/security/CsrfRequestMatcher.java
+++ templates/server/src/main/resources/archetype-resources/core/src/main/java/__packageInPathFormat__/general/common/impl/security/CsrfRequestMatcher.java
@@ -2,6 +2,7 @@ package ${package}.general.common.impl.security;
 
 import java.util.regex.Pattern;
 
+import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -12,6 +13,7 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
  *
  * @see <a href="https://en.wikipedia.org/wiki/Cross-site_request_forgery">Cross-site request forgery</a>
  */
+@Named("CsrfRequestMatcher")
 public class CsrfRequestMatcher implements RequestMatcher {
 
   private static final Pattern HTTP_METHOD_PATTERN = Pattern.compile("^GET$");
