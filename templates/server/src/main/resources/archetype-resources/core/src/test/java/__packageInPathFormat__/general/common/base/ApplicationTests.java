5f31956e51b7d45567f731c45671599cdbcbc192
Suvendu Mandal <suvendu.mandal@capgemini.com>
2021-02-04 16:27:12 +0530

    #323: Add spring integration test to archetype #323

---
+++ templates/server/src/main/resources/archetype-resources/core/src/test/java/__packageInPathFormat__/general/common/base/ApplicationTests.java
@@ -0,0 +1,20 @@
+package ${package}.general.common.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import com.devonfw.module.test.common.base.ModuleTest;
+import com.example.application.sampleapp.SpringBootApp;
+
+/**
+ * This test verifies that the entire code of SpringBootApp.
+ *
+ */
+@SpringBootTest(classes = SpringBootApp.class, webEnvironment = WebEnvironment.MOCK)
+public class ApplicationTests extends ModuleTest {
+  @Test
+  public void contextLoads() {
+
+  }
+}
