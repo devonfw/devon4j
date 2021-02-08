5c8a4b9cec3af7cf098bc2a9233c3f8168f15f88
Jörg Hohwiller <hohwille@users.noreply.github.com>
2021-02-08 10:06:22 +0100

    Update SpringBootAppTest.java

--- templates/server/src/main/resources/archetype-resources/core/src/test/java/__packageInPathFormat__/general/common/base/SpringBootAppTest.java
+++ templates/server/src/main/resources/archetype-resources/core/src/test/java/__packageInPathFormat__/general/common/base/SpringBootAppTest.java
@@ -7,13 +7,12 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import com.devonfw.module.test.common.base.ModuleTest;
 
 /**
- * This test verifies that the entire code of SpringBootApp.
- *
+ * This test verifies that {@link SpringBootApp} is able to startup.
  */
 @SpringBootTest
 public class SpringBootAppTest extends ModuleTest {
   @Test
   public void contextLoads() {
-
+    // just ensure the app can startup
   }
 }
