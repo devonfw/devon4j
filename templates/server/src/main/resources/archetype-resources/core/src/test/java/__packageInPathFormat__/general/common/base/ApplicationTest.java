81738dafc7e93af63d0ff208d5f2787f833d252a
Suvendu Mandal <suvendu.mandal@capgemini.com>
2021-04-15 17:00:18 +0530

    adding assertation
    adding assertation

--- templates/server/src/main/resources/archetype-resources/core/src/test/java/__packageInPathFormat__/general/common/base/ApplicationTest.java
+++ templates/server/src/main/resources/archetype-resources/core/src/test/java/__packageInPathFormat__/general/common/base/ApplicationTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
+import com.devonfw.module.basic.common.api.reflect.Devon4jPackage;
 import com.devonfw.module.test.common.base.ModuleTest;
 import com.example.application.sampleapp.SpringBootApp;
+import com.example.application.sampleapp.general.common.base.Devon4jPackageCheckTest;
 
 /**
  * This test verifies that {@link SpringBootApp} is able to startup.
@@ -13,7 +15,8 @@ import com.example.application.sampleapp.SpringBootApp;
 public class ApplicationTest extends ApplicationComponentTest {
   @Test
   public void contextLoads() {
-    // just ensure the app can startup
-    SpringBootApp.main(new String[] {});
+
+    Devon4jPackage pkg = Devon4jPackage.of(Devon4jPackageCheckTest.class);
+    assertThat(pkg.isValid()).isTrue();
   }
 }
