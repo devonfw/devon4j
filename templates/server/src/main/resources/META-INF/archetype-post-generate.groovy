5afd20acf9a40b0af40fc7f46fcd31dc75225fb9
dixyushi <you@example.com>
2021-06-02 10:15:38 +0530

    handled DatabaseException

--- templates/server/src/main/resources/META-INF/archetype-post-generate.groovy
+++ templates/server/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -3,19 +3,24 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 // the path where the project got generated
-File rootDir = new File(request.outputDirectory + "/" + request.artifactId)
+Path projectPath = Paths.get(request.outputDirectory, request.artifactId)
 
 // the properties available to the archetype
 Properties properties = request.properties
 
-// dbMigration is either liquibase or flyway
+// dbMigration is either flyway or liquibase
 String dbMigration = properties.get("dbMigration")
 
+if (dbMigration == "flyway") {
+  // delete the changelog folder as it is not required for flyway
+  Files.deleteIfExists projectPath.resolve("src/main/resources/db/changelog/changelog-master.xml")
+  Files.deleteIfExists projectPath.resolve("src/main/resources/db/changelog/changelog-v1.0.xml")
+  Files.deleteIfExists projectPath.resolve("src/test/resources/db/changelog/changelog-master.xml")
+  Files.deleteIfExists projectPath.resolve("src/test/resources/db/changelog/changelog-v1.0.xml")
+  
+} else if(dbMigration == "liquibase") {
+  // delete the changelog folder as it is not required for flyway
+   Files.deleteIfExists projectPath.resolve("src/main/resources/db/migration/1.0/V0001_Create_Sequence.sql")
+   Files.deleteIfExists projectPath.resolve("src/main/resources/db/test/V0001_InitDb.sql")
+}
 
-if(dbMigration == "flyway") {
-  // delete the changelog folder as it is not required for flyway
-  new File(rootDir, "src/main/resources/db/changelog").deleteDir()
-}else if(dbMigration == "liquibase") {
-  // delete the changelog folder as it is not required for flyway
-  new File(rootDir, "src/main/resources/db/migration").deleteDir()
-  }
--- templates/server/src/main/resources/archetype-resources/core/src/test/java/__packageInPathFormat__/general/common/base/test/DbTestHelper.java
+++ templates/server/src/main/resources/archetype-resources/core/src/test/java/__packageInPathFormat__/general/common/base/test/DbTestHelper.java
@@ -1,76 +1,36 @@
 package ${package}.general.common.base.test;
 
 import javax.inject.Named;
-#if ($dbMigration == 'liquibase')  
- import liquibase.Liquibase;
-#else 
-import org.flywaydb.core.Flyway;
-#end
+import com.devonfw.module.test.common.base.clean.TestCleanerPlugin;
+import com.devonfw.module.test.common.base.clean.TestCleanerPluginLiquibase;
+import com.devonfw.module.test.common.base.clean.TestCleanerPluginFlyway;
 /**
  * This class provides methods for handling the database during testing where resets (and other operations) may be
  * necessary.
  */
 @Named
-public class DbTestHelper {
- #if ($dbMigration == 'liquibase')  
-   private Liquibase liquibase; 
- #else 
-   private Flyway flyway;
- #end
- 
- #if ($dbMigration == 'liquibase')  
-   /**
-    * The constructor.
-    *
-    * @param liquibase an instance of type {@link Liquibase}.
-    */
-   public DbTestHelper( Liquibase liquibase) {
-     super();
-       this.liquibase = liquibase;
- }
- #else 
-   /**
-    * The constructor.
-    *
-    * @param flyway an instance of type {@link Flyway}.
-    */
-   public DbTestHelper( Flyway flyway) {
-     super();
-       this.flyway = flyway;
- }
- #end
- 
-  #if ($dbMigration == 'flyway')
-  /**
-   * Calls {@link #dropDatabase()} internally, and migrates to the highest available migration (default) or to the
-   * {@code migrationVersion} specified by {@link #setMigrationVersion(String)}.
-   */
-  public void resetDatabase() {
-    
-      this.flyway.migrate();   
-  }
-  #end
+public class DbTestHelper{
 
-#if ($dbMigration == 'liquibase')  
+  
+  private TestCleanerPlugin testCleanerPlugin;
+
+  public DbTestHelper(TestCleanerPlugin testCleanerPlugin) {
+
+    super();
+    this.testCleanerPlugin = testCleanerPlugin;
+  }
+
   /**
    * Drops the whole database.
    */
-  public void cleanup() {
-
-    try {
-      this.liquibase.dropAll();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-  }
- #else
-   /**
-    * Drops the whole database.
-    */
-   public void dropDatabase() {
-       this.flyway.clean();
-   }
+  public void dropDatabase() {
+#if($dbMigration == 'liquibase')
+      testCleanerPlugin = new TestCleanerPluginLiquibase();
+#end 
+#if($dbMigration == 'flyway')
+      testCleanerPlugin = new TestCleanerPluginFlyway();
 #end
+      testCleanerPlugin.cleanup();
+  }
 
 }
