41c183a2c677d08891feda948983eb3786785b9b
Vicaria Barker <rafael.vicaria-barker@capgemini.com>
2022-01-31 11:14:57 +0100

    Removed all unused files

--- modules/test-jpa/src/main/java/com/devonfw/module/test/common/base/clean/TestCleanerPlugin.java
+++
@@ -1,8 +0,0 @@
-package com.devonfw.module.test.common.base.clean;
-
-/**
- * Interface for a single "plugin" of {@link TestCleanerImpl} performing all {@link #cleanup() cleanups}.
- */
-public interface TestCleanerPlugin extends AbstractTestCleaner {
-
-}
--- modules/test-junit4/src/main/java/com/devonfw/module/test/common/api/category/CategoryModuleTest.java
+++
@@ -1,8 +0,0 @@
-package com.devonfw.module.test.common.api.category;
-
-/**
- * This is the JUnit {@link org.junit.experimental.categories.Category}
- */
-public interface CategoryModuleTest {
-
-}
